81fbb170e20b5ce976b625e91a6a29967cb08f43
eri <eri360roperez@gmail.com>
2024-08-24 20:45:21 -0600

    Buttons and events

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_getCityId, btn_getWeatherByCityId, btn_getWeatherByCityName;
+    EditText et_dataInput;
+    ListView lv_weatherReports;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.et_dataInput), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // assign values to each element in layout
+        btn_getCityId = findViewById(R.id.btn_getCityId);
+        btn_getWeatherByCityId = findViewById(R.id.btn_getWeatherByCityId);
+        btn_getWeatherByCityName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+
+        lv_weatherReports = findViewById(R.id.lv_weatherReports);
+
+        // click listeners for each button
+        btn_getCityId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "some text", Toast.LENGTH_SHORT );
+            }
+        });
+
+        btn_getWeatherByCityId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "some text", Toast.LENGTH_SHORT );
+            }
+        });
+
+        btn_getWeatherByCityName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "some text" + et_dataInput.getText().toString(), Toast.LENGTH_SHORT );
+            }
+        });
+
+
     }
 }
